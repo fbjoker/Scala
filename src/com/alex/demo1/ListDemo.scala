74229ea76c691e160e2870358738c78dcb176612
fbjoker <39256912@qq.com>
2018-12-15 13:52:51 +0800

    day2

--- src/com/alex/demo1/ListDemo.scala
+++ src/com/alex/demo1/ListDemo.scala
@@ -2,4 +2,18 @@ package com.alex.demo1
 
 object ListDemo {
 
+  def main(args: Array[String]): Unit = {
+
+
+    var n=22
+
+
+    var arr=for (x<- 0 until n) yield   math.random*n
+    arr.toArray
+
+    println(arr)
+    println(arr)
+
+  }
+
 }
