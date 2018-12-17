20cfd21f05cfa77db4e6568c8e1f4209cbc8cde3
fbjoker <39256912@qq.com>
2018-12-17 14:05:43 +0800

    day4

--- src/com/alex/demo1/demo2/Testdemo.scala
+++ src/com/alex/demo1/demo2/Testdemo.scala
@@ -2,6 +2,7 @@ package com.alex.demo1.demo2
 
 object Testdemo {
   def main(args: Array[String]): Unit = {
+/*
     var p1= new Person();
 
     p1.age=10
@@ -11,6 +12,16 @@ object Testdemo {
 
     println(p1.getName)
     println(p1.age)
+
+
+    var emp= new Emp()
+
+    emp.name="alex"
+    emp.age=18
+
+    println(emp.getName+"\t"+emp.age)
+
+*/
 
 
 
