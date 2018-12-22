5c00a78ac3f63e320a8a1fa1760bfea5fadb0360
fbjoker <39256912@qq.com>
2018-12-22 12:00:24 +0800

    day6

--- src/com/alex/demo1/demo6/TestFile.scala
+++ src/com/alex/demo1/demo6/TestFile.scala
@@ -2,6 +2,8 @@ package com.alex.demo1.demo6
 
 import java.io.PrintWriter
 
+import scala.collection.mutable
+import scala.collection.mutable._
 import scala.io.{BufferedSource, Source}
 
 class TestFile {
@@ -13,11 +15,11 @@ object TestFile{
 
 
   def main(args: Array[String]): Unit = {
-    val source: BufferedSource = Source.fromFile("D:\\HadoopCluster\\Hadoopdata\\video\\3.txt")
-
-    val lin: Iterator[String] = source.getLines()
-
-    println(lin)
+//    val source: BufferedSource = Source.fromFile("D:\\HadoopCluster\\Hadoopdata\\video\\3.txt")
+//
+//    val lin: Iterator[String] = source.getLines()
+//
+//    println(lin)
 //    lin.foreach(println(_))
 
 //    val data: Array[String] = source.mkString.split("\\s+")
@@ -30,6 +32,34 @@ object TestFile{
 //
 //
 //    out.map(x=>writer.write((""+x._1+"\t"+x._2+"\n")))
+
+    val map = new scala.collection.mutable.HashMap[String,Int]()
+
+    map++=Map("id"->1001,"name"->2222)
+    map++=Map("galax"->444)
+    map++=Map("galax"->444)
+    val array: Array[(String, Int)] = map.toArray
+    val ttt: HashMap[String, Int] = map.map(x=>(x._1,x._2))
+    val a = Map("e" -> 1, "a" -> 1, "b" -> 1, "c" -> 2, "d" -> 1).toArray
+    val tt: mutable.Map[String, ArrayBuffer[(String, Int)]] = Map("a" -> ArrayBuffer(("e",1), ("e",1)))
+
+    //tt.mapValues(x=>x.reduce((a,b)=>{a._2+b._2}))
+
+    val i= Array(("e",1), ("e",1)).reduceLeft((a, b)=>{(a._1,a._2+b._2)})
+    val tte= Array(("e",1), ("e",1))
+    var count=0
+    for (x<-tte){
+      count += x._2
+
+    }
+
+
+
+    println(map)
+    println(array.toBuffer)
+    println(a.toBuffer)
+    println(count)
+
 
 
   }
